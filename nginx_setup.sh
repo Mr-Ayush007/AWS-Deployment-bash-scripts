a89e45b09276e222229ac08cf4e1841385933147
Mr-Ayush007 <ayush07malviya07@gmail.com>
2026-04-15 02:59:00 +0530

    Change editor from nano to gedit for nginx config

--- nginx_setup.sh
+++ nginx_setup.sh
@@ -3,4 +3,4 @@ cd ~
 sudo apt install nginx -y
 sudo rm /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
-sudo nano /etc/nginx/sites-available/nginx_conf
+sudo gedit /etc/nginx/sites-available/nginx_conf
