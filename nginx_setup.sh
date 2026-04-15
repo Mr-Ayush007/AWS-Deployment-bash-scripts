d35107949e7e43331ba2bbb75233ff48a0ada678
Mr-Ayush007 <ayush07malviya07@gmail.com>
2026-04-15 12:33:42 +0530

    Change text editor from gedit to vim in setup script

--- nginx_setup.sh
+++ nginx_setup.sh
@@ -3,4 +3,4 @@ cd ~
 sudo apt install nginx -y
 sudo rm /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
-sudo gedit /etc/nginx/sites-available/nginx_conf
+sudo vim /etc/nginx/sites-available/nginx_conf
